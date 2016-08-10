59e4495e7e410071e67bddaf6f50175b59f2b95a
Rinat Yuldashbaev <rinatoptimus@gmail.com>
2016-08-10 22:01:44 +0300

    Built a nested component that passes in & out a data (using @Input, @Output).

--- app/products/product-list.component.ts
+++ app/products/product-list.component.ts
@@ -1,14 +1,17 @@
 import {Component, OnInit} from 'angular2/core';
 import {IProduct} from './product';
-import {ProductFilterPipe} from './product-filter.pipe'
+import {ProductFilterPipe} from './product-filter.pipe';
+import {StarComponent} from '../shared/star.component';
+
 @Component({
     selector: 'pm-products',
     templateUrl: 'app/products/product-list.component.html',
     styleUrls: ['app/products/product-list.component.css'],
-    pipes: [ProductFilterPipe]
+    pipes: [ProductFilterPipe],
+    directives: [StarComponent]
 })
 export class ProductListComponent implements OnInit {
-    pageTitle: string = 'Product List?';
+    pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
@@ -72,5 +75,8 @@ export class ProductListComponent implements OnInit {
 
     ngOnInit(): void {
         console.log('In OnInit');
+    }
+    onRatingClicked(message: string) : void {
+        this.pageTitle = 'Product List: ' + message;
     }
 }

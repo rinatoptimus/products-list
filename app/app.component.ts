f43381e77cb040ca816de661c471e47d7df579fa
Rinat Yuldashbaev <rinatoptimus@gmail.com>
2016-08-10 23:13:49 +0300

    Services and Dependency Injection.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,7 @@
 import {Component} from 'angular2/core';
 import {ProductListComponent} from './products/product-list.component';
+import {ProductService} from './products/product.service';
+
 @Component({
     selector: 'pm-app',
     template: `
@@ -7,7 +9,8 @@ import {ProductListComponent} from './products/product-list.component';
             <pm-products></pm-products>   
         </div>
     `,
-    directives: [ProductListComponent]
+    directives: [ProductListComponent],
+    providers: [ProductService]
 })
 export class AppComponent {
     pageTitle: string = 'Acme Product Management';
